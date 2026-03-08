bd14700bfc55751e5579964c3bcc4f965bc4dc85
Stefdev13 <stef.bekaert@proton.me>
2026-03-08 15:36:53 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ def main():
     # they'll be visible when running tests.
     
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    client_socket = server.accept() # wait for client
-
-    client_socket.sendall(b"\x00\x00\x00\x04")
-    client_socket.sendall(b"\x00\x00\x00\x07")
+    conn, _ = server.accept()  # wait for client
+    with conn:
+        _ = conn.recv(1024)
+        conn.sendall(b"\x00\x00\x00\x04\x00\x00\x00\x07")
 
 
 
